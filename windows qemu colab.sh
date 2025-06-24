c1f625aa2166c0d4537d4efc554a270fae3b48f9
20Qhuy <118644275+20Qhuy@users.noreply.github.com>
2025-06-24 18:22:32 +0700

    Add files via upload

---
+++ windows qemu colab.sh
@@ -0,0 +1,19 @@
+curl -sSL https://ngrok-agent.s3.amazonaws.com/ngrok.asc \
+  | tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null \
+  && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" \
+  | tee /etc/apt/sources.list.d/ngrok.list \
+  && apt update \
+  && apt install ngrok > /dev/null 2>&1
+ngrok config add-authtoken 2MWv85utwyi9ItXxwvdn948ytGR_7q5h1JLXmpMiyW1v4mXZn  
+nohup ngrok tcp 5900 &>/dev/null &
+echo Please wait for installing new 10...
+ apt update -y > /dev/null 2>&1
+echo "Installing QEMU (2-3m)..."
+apt install qemu-kvm curl -y > /dev/null 2>&1
+echo "Windows datnguyen On Google Colab"
+echo Your VNC IP Address:
+curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
+echo "Note: Use Right-Click Or Ctrl+C To Copy"
+echo "Please Keep Colab Tab Open, Maximum Time 12h"
+echo Starting Windows xxxz...
+qemu-system-x86_64 -M q35,nvdimm=on -device qemu-xhci -device usb-tablet -device usb-kbd -cpu qemu64,+sse,+sse2,+sse4.1,+sse4.2,+pae,hv-relaxed -smp sockets=1,cores=4,threads=2 -overcommit mem-lock=off -m 8048M,slots=8,maxmem=8049M -object memory-backend-ram,size=1006M,id=m0 -numa node,nodeid=0,memdev=m0,cpus=0 -object memory-backend-ram,size=1006M,id=m1 -numa node,nodeid=1,memdev=m1,cpus=1 -object memory-backend-ram,size=1006M,id=m2 -numa node,nodeid=2,memdev=m2,cpus=2 -object memory-backend-ram,size=1006M,id=m3 -numa node,nodeid=3,memdev=m3,cpus=3 -object memory-backend-ram,size=1006M,id=m4 -numa node,nodeid=4,memdev=m4,cpus=4 -object memory-backend-ram,size=1006M,id=m5 -numa node,nodeid=5,memdev=m5,cpus=5 -object memory-backend-ram,size=1006M,id=m6 -numa node,nodeid=6,memdev=m6,cpus=6 -object memory-backend-ram,size=1006M,id=m7 -numa node,nodeid=7,memdev=m7,cpus=7 -drive file=win.qcow2,aio=threads,cache=writeback -cdrom win.iso -vga vmware -device cs4231a,audiodev=add -audiodev sdl,id=add -device i82558b,netdev=n0 -netdev user,id=n0 -accel tcg,thread=multi,tb-size=2048 -device virtio-balloon-pci -device intel-iommu -monitor stdio -vnc :0
