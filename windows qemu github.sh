8d5022b518220e44f1dee7eb4687f235d921e56d
20Qhuy <118644275+20Qhuy@users.noreply.github.com>
2025-06-28 21:28:34 +0700

    Update windows qemu github.sh

--- windows qemu github.sh
+++ windows qemu github.sh
@@ -16,4 +16,4 @@ curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*publ
 echo "Note: Use Right-Click Or Ctrl+C To Copy"
 echo "Please Keep Colab Tab Open, Maximum Time 12h"
 echo Starting Windows xxxz...
-qemu-system-x86_64 -M q35 -usb -device qemu-xhci -device usb-tablet -device usb-kbd -cpu qemu64,+sse,+sse2,+sse4.1,+sse4.2,+pae,hv-relaxed -smp sockets=1,cores=4,threads=1 -m 4G  -drive format=raw,file=win.img -vga std -device ich9-intel-hda -device hda-duplex -device e1000e,netdev=n0 -netdev user,id=n0 -accel tcg -device virtio-serial-pci -boot d,menu=on -device intel-iommu -vnc :0
+qemu-system-x86_64 -cpu qemu64 -drive format=raw,file=win.img  -smp cores=2  -m 8G -machine usb=on -device usb-tablet -accel tcg -vnc :0
