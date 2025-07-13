5d3df373de0fc88210a18ed29f6861220a21bed7
20Qhuy <118644275+20Qhuy@users.noreply.github.com>
2025-07-13 20:21:06 +0700

    Update vpsfirebase.sh

--- vpsfirebase.sh
+++ vpsfirebase.sh
@@ -9,5 +9,4 @@ nohup apt qemu-kvm -y  >/dev/null 2>&1 &
 echo "vps ip"
 tailscale ip --4
 echo "Administrator:Datnguyentv.com"
-nohup 
-qemu-system-x86_64 -M q35 -usb -device qemu-xhci -device usb-tablet -device usb-kbd -cpu host,+pae -smp sockets=1,cores=8,threads=1 -m 32G -drive file=win.qcow2,aio=threads,cache=writeback,if=none,id=hda -device ahci,id=hdaahci -device ide-hd,drive=hda,bus=hdaahci.0 -vga std -device ich9-intel-hda -device hda-duplex -device e1000e,netdev=n0 -netdev user,id=n0,hostfwd=tcp::3389-:3389 -accel kvm -device virtio-serial-pci -device intel-iommu -vnc :0  >/dev/null 2>&1 &  
+nohup qemu-system-x86_64 -M q35 -usb -device qemu-xhci -device usb-tablet -device usb-kbd -cpu host,+pae -smp sockets=1,cores=8,threads=1 -m 32G -drive file=win.qcow2,aio=threads,cache=writeback,if=none,id=hda -device ahci,id=hdaahci -device ide-hd,drive=hda,bus=hdaahci.0 -vga std -device ich9-intel-hda -device hda-duplex -device e1000e,netdev=n0 -netdev user,id=n0,hostfwd=tcp::3389-:3389 -accel kvm -device virtio-serial-pci -device intel-iommu -vnc :0  >/dev/null 2>&1 &  
